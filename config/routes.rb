c84245c96b8506f458c08f3c74c3a4b1675e9d49
Frank Chung <ponty33@gmail.com>
2018-10-20 01:08:35 +0000

    setup home

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
 
-  # root to: 'home#index'
+  root to: 'home#index'
 
-  # resources :parents
+  resources :parents, only: [:index] do
+    resources :admissions, only: [:index, :new, :create]
+    resources :reviews, only: [:new, :create]
+    resources :assignments, only: [:index]
+    resources :reports, only: [:index]
+  end
   # resources :admins
   # resources :teachers
 
-  # get '/register', to: 'parent#create'
+  get '/register', to: 'parents#new'
+  post '/parents', to: 'parents#create'
+
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  get '/logout', to: 'session#destroy'
 
 end
