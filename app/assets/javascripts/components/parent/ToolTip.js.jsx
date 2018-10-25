96c9ff8cce88e20b2e296dabad8fee747254ff38
Charles Wong <nok.wong@gmail.com>
2018-10-25 20:56:18 +0000

    add student performance chart

--- app/assets/javascripts/components/parent/ToolTip.js.jsx
+++ app/assets/javascripts/components/parent/ToolTip.js.jsx
@@ -3,19 +3,21 @@
 class ToolTip extends React.Component {
 
   render() {
-    const {hoverLoc, activePoint} = this.props;
+    const { hoverLoc, activePoint } = this.props;
     const svgLocation = document.getElementsByClassName("svg_graph")[0].getBoundingClientRect();
 
     let placementStyles = {};
     let width = 150;
+    placementStyles.height = 110 + 'px';
     placementStyles.width = width + 'px';
-    placementStyles.left = hoverLoc + svgLocation.left - (width/2);
+    placementStyles.left = hoverLoc + svgLocation.left - (width / 2);
 
     return (
-      <div className='hover' style={ placementStyles }>
-        <div className='date'>{`${activePoint.startDate} - ${activePoint.endDate}`}</div>
-        <div className='name'>{ activePoint.name }</div>
-        <div className='feedback'>{activePoint.feedback }</div>
+      <div className='hover' style={placementStyles}>
+        <div className='name'>{activePoint.name}</div>
+        <div className='date'>{activePoint.endDate}</div>
+
+        <div className='feedback'>{activePoint.feedback}</div>
       </div>
     )
   }
--- app/assets/javascripts/components/parent/_report.js.jsx
+++ app/assets/javascripts/components/parent/_report.js.jsx
@@ -9,7 +9,7 @@ class Report extends React.Component {
       console.log("score is ", assignments[x].score);
       let y = Number(assignments[x].score)
       const temp = data.length > 0 ? data[data.length - 1].y : 50;
-      data.push({ x, y, score: x, name: assignments[x].assignment, startDate: assignments[x].start_date,endDate: assignments[x].end_date,feedback: assignments[x].feedback  })
+      data.push({ x, y, score: x, name: assignments[x].name, startDate: assignments[x].start_date, endDate: assignments[x].end_date, feedback: assignments[x].feedback })
     }
     console.log("data is ", data);
     return data;
@@ -60,24 +60,29 @@ class Report extends React.Component {
     return (
       <div className='container'>
         <br></br>
-        <div className="jumbotron" id="report">
-          <h1>report</h1>
-          <br></br>
-          <img src={asset_paths.chart} />
+        <div id="report">
+          <span ><h1>report</h1></span><span><img src={asset_paths.chart} /></span>
+
         </div>
 
         <div className="graph-container">
           <div>100%</div>
           <div className='row'>
-            {<div className='popup'>
+            <div className='popup'>
               {this.state.hoverLoc ? <ToolTip hoverLoc={this.state.hoverLoc} activePoint={this.state.activePoint} /> : null}
-            </div>}
+            </div>
           </div>
+
+
           <div className='row'>
             <div id="linechart" className='chart'>
               {reports}
             </div>
           </div>
+
+
+
+
         </div>
         <div>0</div>
       </div>
