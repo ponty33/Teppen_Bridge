293c288283ced4ee9e5962b21297dd4e55936974
nicholasyyu <nicholas.yu24@gmail.com>
2018-10-25 22:46:45 +0000

    fixed teacher photo under teacher review page

--- app/assets/javascripts/components/teacher/_treview.js.jsx
+++ app/assets/javascripts/components/teacher/_treview.js.jsx
@@ -2,8 +2,10 @@ class Treview extends React.Component{
   
   constructor(props) {
     super(props);
+    image = ""
     this.state = {
-      reviews: []
+      reviews: [],
+      teacher: asset_paths[image]
     };
 
   }
@@ -14,10 +16,12 @@ class Treview extends React.Component{
         return response.json();
     })
     .then((data) => {
-      console.log("Data incoming...");
-      this.setState({ reviews: data })
+      console.log("Data review incoming...",data);
+      this.setState({ reviews: data[0],
+        teacher: asset_paths[data[1].toLowerCase()] })
       console.log("after setting state ");
     })
+    console.log(this.state.teacher);
   }
   
 
@@ -40,7 +44,7 @@ class Treview extends React.Component{
         <div className="jumbotron">
           <h1>Your feedback from our parents!</h1>
           <br></br>
-          <img className="teacher_image"src={asset_paths.yang} />
+          <img className="teacher_image" src={this.state.teacher} />
         </div>
 
         {reviews}
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,14 @@
 class ReviewsController < ApplicationController
 
   def index
-    reviews = Review.joins(:parent).joins(:subject).select('reviews.id, parents.name, content, rating').where(teacher_id: params[:teacher_id])
+    review_teacher = [];
+    review = Review.joins(:parent).joins(:subject).select('reviews.id, parents.name, content, rating').where(teacher_id: params[:teacher_id])
+    review_teacher.push(review);
+    teacher = Teacher.find_by(id: params[:teacher_id])
+    puts "found #{teacher.name}"
 
-    render json: reviews
+    review_teacher.push(teacher.name);
+    render json: review_teacher
   end
 
   def new
