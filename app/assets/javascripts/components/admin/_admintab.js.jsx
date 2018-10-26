cecf9811aa7f62344a9893b7d4b06039f9947c63
Charles Wong <nok.wong@gmail.com>
2018-10-26 20:57:32 +0000

    changes on tabs css

--- app/assets/javascripts/components/admin/_admintab.js.jsx
+++ app/assets/javascripts/components/admin/_admintab.js.jsx
@@ -6,11 +6,12 @@ const Admintab = (props) => {
   return (
     <div id="admin_tabs">
       {/* <button value='profile' className="tablinks" onClick={onclickhandler}>Profile</button> */}
-      <button value='reports' type="button" id="tab_admin" className="btn btn-outline-primary" onClick={onclickhandler}>Report</button>
-      <button value='teachers' type="button" id="tab_admin" className="btn btn-outline-primary" onClick={onclickhandler}>Teachers</button>
-      <button value='subjects' type="button" id="tab_admin" className="btn btn-outline-primary" onClick={onclickhandler}>Subjects</button>
-      <button value='programs' type="button" id="tab_admin" className="btn btn-outline-primary" onClick={onclickhandler}>Programs</button>
-      <button value='parents' type="button" id="tab_admin" className="btn btn-outline-primary" onClick={onclickhandler}>Parents</button>
+      <button value='reports' type="button" id="tab_admin" className="btn btn-outline-secondary" onClick={onclickhandler}>Report</button>
+      <button value='teachers' type="button" id="tab_admin" className="btn btn-outline-secondary" onClick={onclickhandler}>Teachers</button>
+      <button value='subjects' type="button" id="tab_admin" className="btn btn-outline-secondary" onClick={onclickhandler}>Subjects</button>
+      <button value='programs' type="button" id="tab_admin" className="btn btn-outline-secondary" onClick={onclickhandler}>Programs</button>
+      <button value='parents' type="button" id="tab_admin" className="btn btn-outline-secondary" onClick={onclickhandler}>Parents</button>
+      <button type="button" id="tab_admin" className="btn btn-outline-secondary" disabled>admin</button>
     </div>
   )
 }
